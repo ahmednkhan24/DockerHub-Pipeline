88ac53e7b7f0a70918c24562898e67eb1ab0d9a1
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-17 18:59:27 -0500

    added post route

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
 import express from 'express';
+import sanitizer from 'express-sanitizer';
+import bodyParser from 'body-parser';
 import dotenvSafe from 'dotenv-safe';
 
 dotenvSafe.config({ path: '.env' });
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(sanitizer());
+
+const data = [];
+
+const isObjectEmpty = (obj) => Object.entries(obj).length === 0;
 
 app.get('/', (req, res) => {
-  res.status(200).json({ hello: 'world' });
+  res.status(200).json({ 
+    success: true,
+    count: data.length,
+    data: data,
+  });
 });
+
+app.post('/', (req, res) => {
+  if (isObjectEmpty(req.body) || !req.body.input) {
+    res.status(400).json({
+      success: false,
+      message: 'Provide input',
+    });
+  }
+  const input = req.sanitize(req.body.input);
+  data.push(input);
+  res.status(201).json({
+    success: true,
+    count: data.length,
+    input: input,
+  });
+});
+
 
 export default app;
