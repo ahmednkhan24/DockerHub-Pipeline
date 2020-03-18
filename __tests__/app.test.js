3ce6024d05183f1c0faf181896a1a5e18207373d
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-17 19:37:01 -0500

    post unit test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,10 +7,22 @@ const request = supertest(app);
 describe('API endpoints', () => {
   it('should GET root route', async (done) => {
     const response = await request.get('/');
+
     expect(response.status).toBe(200);
     expect(response.body.success).toBe(true);
     expect(response.body.count).toEqual(0);
     expect(response.body.data).toEqual([]);
+    done();
+  });
+
+  it('should POST root route - SUCCESS', async (done) => {
+    const body = { payload: 'hello world' };
+    const response = await request.post('/').send(body);
+    
+    expect(response.status).toBe(201);
+    expect(response.body.success).toBe(true);
+    expect(response.body.count).toEqual(1);
+    expect(response.body.input).toEqual(body.payload);
     done();
   });
 });
