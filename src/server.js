b885983dcbc3bb1e43c2a4a9c1acc676caea670c
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-18 18:19:33 -0500

    app to api

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-import app from './api';
+import api from './api';
 
 const startServer = () => {
   const PORT = process.env.PORT || 3000;
   const IP = process.env.IP || '127.0.0.1';
 
-  const server = app.listen(PORT, IP, () => {
+  const server = api.listen(PORT, IP, () => {
     console.log('API Server has started');
     const { address, port } = server.address();
     console.log(`running at http://${address}:${port}`);
