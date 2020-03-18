499db0641b1ffffeeba287b8d13ba6e988acdcbf
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-17 20:09:06 -0500

    put route done

--- src/app.js
+++ src/app.js
@@ -29,12 +29,37 @@ app.post('/', async (req, res) => {
       message: 'Provide input',
     });
   }
-  const input = req.sanitize(req.body.payload);
+  const input = req.sanitize(req.body.payload).toString();
   data.push(input);
   return res.status(201).json({
     success: true,
     count: data.length,
     input: input,
+  });
+});
+
+app.put('/', async (req, res) => {
+  if (isObjectEmpty(req.body) || !req.body.oldPayload || !req.body.newPayload) {
+    return res.status(400).json({
+      success: false,
+      message: 'Provide input',
+    });
+  }
+  const valueToFind = req.sanitize(req.body.oldPayload).toString();
+  const changeTo = req.sanitize(req.body.newPayload).toString();
+  const index = data.indexOf(valueToFind);
+  if (index === -1) {
+    return res.status(500).json({
+      success: false,
+      message: 'Not found',
+    });
+  }
+  data[index] = changeTo;
+  return res.status(200).json({
+    success: true,
+    count: data.length,
+    oldValue: valueToFind,
+    newValue: data[index],
   });
 });
 
