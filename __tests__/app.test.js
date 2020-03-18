d64c7ecb4df298f394759f96bb462c5532546c20
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-17 19:01:30 -0500

    started working on unit tests

---
+++ __tests__/app.test.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+//import polyfill from 'babel-polyfill';
+import supertest from 'supertest';
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true);
+  });
+});
+
+describe('GET endpoints', () => {
+  it('should GET root route', async (done) => {
+    const response = await request.get('/');
+    expect(response.status).toBe(200);
+    expect(response.body.hello).toBe('world');
+    done();
+  });
+});
