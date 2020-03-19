181a0629e73a9b1c1ef0e2215c3e09a196d5a98f
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-18 19:37:36 -0500

    changed order of seed

--- src/routes.js
+++ src/routes.js
@@ -13,9 +13,11 @@ import {
 const router = express.Router();
 
 router.route('/').get(getRoot);
-router.route('/seed').post(seedData).delete(purgeData);
+
 router.route('/data').get(getData).post(postData);
 router.route('/data/:payload').put(putData).delete(deleteData);
+
+router.route('/seed').post(seedData).delete(purgeData);
 router.route('*').get(notFound).post(notFound).put(notFound).delete(notFound);
 
 export default router;
