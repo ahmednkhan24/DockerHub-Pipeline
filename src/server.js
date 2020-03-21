03fb81414ff484e37fc11553decccf03cbc6c7d8
ahmednkhan24 <ahmednkhan24@gmail.com>
2020-03-21 16:46:33 -0500

    server consoles

--- src/server.js
+++ src/server.js
@@ -4,10 +4,9 @@ const startServer = () => {
   const PORT = process.env.PORT || 3000;
   const IP = process.env.IP || '127.0.0.1';
 
-  const server = api.listen(PORT, IP, () => {
-    console.log('API Server has started');
-    const { address, port } = server.address();
-    console.log(`running at http://${address}:${port}`);
+  const server = api.listen(PORT, () => {
+    console.log('API Server has Started');
+    console.log(`running at http://${IP}:${PORT}`);
   });
 };
 
